b2806e1e5afd5fe57d87611f955110e199c351dd
Miguel Teran <miguel.teranj02@gmail.com>
2022-10-18 18:51:46 -0400

    oferta habilitada 21/10/22

--- js/countdown.js
+++ js/countdown.js
@@ -1,10 +1,10 @@
 simplyCountdown("#discount-container", {
   year: 2022, // required
   month: 10, // required
-  day: 13, // required
-  hours: 0, // Default is 0 [0-23] integer
-  minutes: 0, // Default is 0 [0-59] integer
-  seconds: 0, // Default is 0 [0-59] integer
+  day: 21, // required
+  hours: 24, // Default is 0 [0-23] integer
+  minutes: 60, // Default is 0 [0-59] integer
+  seconds: 60, // Default is 0 [0-59] integer
   words: {
     //words displayed into the countdown
     days: "Día",
